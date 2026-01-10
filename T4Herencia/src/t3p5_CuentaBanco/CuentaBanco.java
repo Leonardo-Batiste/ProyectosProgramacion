93ae4f733f9e7e087404b6a75b71c508b118a19f
leo-a11y46 <leonardo.batiste.estudios@gmail.com>
2026-01-10 12:04:06 +0100

    Añadido javadoc a la clase CuentaBanco

--- T4Herencia/src/t3p5_CuentaBanco/CuentaBanco.java
+++ T4Herencia/src/t3p5_CuentaBanco/CuentaBanco.java
@@ -1,22 +1,51 @@
 package t3p5_CuentaBanco;
-/**
- * 
- */
+
 import java.util.*;
+
+/**
+ * Cuenta bancaria con operaciones basicas
+ */
 public class CuentaBanco {
-	//Propiedades
-	public String 	numero,
-					titular;
+	/**
+	 * Propiedades de los objetos
+	 */
+	
+	/**
+	 * numero = numero de la cuenta
+	 */
+	public String 	numero;
+					
+	public String titular;
 	
 	public double saldo;
 	
+	/**
+	 * true = la cuenta esta bloqueada
+	 * false = la cuenta esta desbloqueada
+	 */
 	public boolean bloqueada;
 	
+	/**
+	 * numeroMaximoCuentas = Si se ha llegado al tope de cuentas, aumentar esta propiedad de la clase
+	 */
 	public static final int numeroMaximoCuentas=1000;
 	
-	public static int numeroCuenta=1;	
+	/**
+	 * @param numeroActualCuenta = El numero en el que empieza las cuentas
+	 */
+	public static int numeroActualCuenta=1;	
 	
-	//Constructores
+	/**
+	 * Constructores de la clase CuentaBanco
+	 */
+	
+	/**
+	 * Constructor que crea el objeto con el numero de cuenta y el titular introducidos manualmente
+	 * @param numCuenta = Numero de cuenta introducida manualmente
+	 * @param titularCuenta = El nombre del titular de la cuenta
+	 * bloqueada = Las cuentas estan desbloqueadas al crearse
+	 * saldo = Las cuentas empiezan con $0 de saldo
+	 */
 	public CuentaBanco(int numCuenta, String titularCuenta) {
 		this.numero=""+numCuenta;
 		this.titular=titularCuenta;
@@ -24,34 +53,54 @@ public class CuentaBanco {
 		this.saldo=0.0;
 	}
 	
-	public CuentaBanco(String titularCuenta) { //Este constructor genera el numero de cuenta automaticamente sin repetir
-		if (numeroCuenta>numeroMaximoCuentas) {
+	/**
+	 * Este constructor genera el numero de la cuenta de forma automatica, solo se necesita introducir el nombre para el titular de la cuenta
+	 * @param titularCuenta = El nombre del titular de la cuenta
+	 * bloqueada = Las cuentas estan desbloqueadas al crearse
+	 * saldo = Las cuentas empiezan con $0 de saldo
+	 */
+	public CuentaBanco(String titularCuenta) {
+		if (numeroActualCuenta>numeroMaximoCuentas) {
 			System.out.println("No se pueden crear mas cuentas, se ha alcanzado el maximo.");
 		}
-		this.numero=""+numeroCuenta++;
+		this.numero=""+numeroActualCuenta++;
 		this.titular=titularCuenta;
 		this.bloqueada=false;
 		this.saldo=0.0;
 	}
 	
-	//Metodos
+	/**
+	 * Metodos de la clase CuentaBanco
+	 */
+	
+	/**
+	 * consultarSaldo funciona estando bloqueada la cuenta
+	 * @return un double con el saldo actual de la cuenta
+	 */
 	public double consultarSaldo() {
-		//Funciona estando bloqueada la cuenta
 		return this.saldo;
 	}
 	
-	void ingresarDinero(double cantidad) {
+	/**
+	 * Ingresa X dinero a this.saldo si no esta bloqueada la cuenta
+	 * @param cantidad = cantidad que se añade
+	 */
+	public void ingresarDinero(double cantidad) {
 		if (this.bloqueada) {
 			System.out.println("La cuenta esta bloqueada, no puedes ingresar dinero.");
 		}
 		else {
-			System.out.println("Has ingresado "+cantidad+" a tu cuenta "+numero);
+			System.out.println("Has ingresado $"+cantidad+" de la cuenta nº "+numero);
 			this.saldo+=cantidad;
 		}
 		
 	}
 	
-	void retirarDinero(double cantidad) {
+	/**
+	 * Resta dinero de this.saldo, en caso de que no este bloqueada, y tengas suficiente saldo para retirar esa cantidad
+	 * @param cantidad = Cantidad a retirar
+	 */
+	public void retirarDinero(double cantidad) {
 		if (this.bloqueada) {
 			System.out.println("La cuenta esta bloqueada, no puedes retirar dinero.");
 		}
@@ -60,11 +109,15 @@ public class CuentaBanco {
 		}
 		else {
 			this.saldo-=cantidad;
-			System.out.println("Has retirado "+cantidad+"$ de tu cuenta "+this.numero);
+			System.out.println("Has retirado $"+cantidad+" de la cuenta nº "+this.numero);
 		}
 	}
 	
-	void cambioTitular(String nuevoTitular) {
+	/**
+	 * Reemplaza el nombre del titular actual de la cuenta por uno nuevo, en caso de que no este bloqueada y comprueba que tenga caracteres el nuevo titular
+	 * @param nuevoTitular = Nombre del nuevo titular de la cuenta
+	 */
+	public void cambioTitular(String nuevoTitular) {
 		if (this.bloqueada) {
 			System.out.println("La cuenta esta bloqueada, no puedes cambiar de titular.");
 		}
@@ -73,11 +126,14 @@ public class CuentaBanco {
 		}
 		else{
 			this.titular=nuevoTitular;
-			System.out.println("El nuevo titular de la cuenta "+this.numero+" es"+nuevoTitular);
+			System.out.println("El nuevo titular de la cuenta "+this.numero+" es "+nuevoTitular);
 		}
 	}
 	
-	void bloquear() {
+	/**
+	 * bloquea la cuenta
+	 */
+	public void bloquear() {
 		if (this.bloqueada) {
 			System.out.println("No puedes bloquear la cuenta, ya esta bloqueada.");
 		}
@@ -88,19 +144,31 @@ public class CuentaBanco {
 		
 	}
 	
+	/**
+	 * Desbloquea la cuenta, funciona estando bloqueada
+	 */
 	void desbloquear() {
-		//Funciona estando bloqueada la cuenta
 		this.bloqueada=false;
 		System.out.println("La cuenta "+this.numero+" se ha desbloqueado.");
 	}
 	
+	/**
+	 * Imprime las propiedades del objeto, funciona estando bloqueada la cuenta
+	 */
 	void imprimirDatos() {
-		//Funciona estando bloqueada la cuenta
-		System.out.println("-----Has seleccionado imprimir los datos de la cuenta-----");
+		System.out.println("\n-----Has seleccionado imprimir los datos de la cuenta-----");
+		
 		System.out.println("El numero de la cuenta es: "+this.numero);
 		System.out.println("El titular de la cuenta es: "+this.titular);
-		System.out.println("El saldo de la cuenta es "+this.saldo);
-		System.out.println("¿Esta bloqueada?"+this.bloqueada);
-		System.out.println("----------------------------------------------------------");
+		System.out.println("El saldo de la cuenta es $"+this.saldo);
+		
+		if (this.bloqueada) {
+			System.out.println("La cuenta nº "+this.numero+" esta bloqueada");
+		}
+		else {
+			System.out.println("La cuenta nº "+this.numero+" no esta bloqueada");
+		}
+		
+		System.out.println("----------------------------------------------------------\n");
 	}
 }
--- T4Herencia/src/t3p5_CuentaBanco/PruebaCuentaBanco.java
+++ T4Herencia/src/t3p5_CuentaBanco/PruebaCuentaBanco.java
@@ -2,24 +2,13 @@ package t3p5_CuentaBanco;
 
 public class PruebaCuentaBanco {
 	public static void main(String[] args) {
-		CuentaBanco cb = new CuentaBanco(1111, "joseluis");
+		CuentaBanco cb = new CuentaBanco("joseluis");
 		
+		CuentaBanco cb2 = new CuentaBanco("Gonzalo");
+		
+		//El problema pedia que consultarSaldo solo devolviese un numero, necesita un syso para imprimirlo
 		System.out.println("El saldo de la cuenta es: "+cb.consultarSaldo());
 		
-		cb.ingresarDinero(500);
 		
-		cb.retirarDinero(400);
-		
-		cb.imprimirDatos();
-		
-		cb.bloquear();
-		
-		cb.bloquear();
-		
-		cb.desbloquear();
-		
-		cb.cambioTitular("JoseFina");
-		
-		cb.imprimirDatos();
 	}
 }
