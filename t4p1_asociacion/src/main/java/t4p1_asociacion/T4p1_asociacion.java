795571d6ecf3c7a95ec5087f8091a6817d6dc8ff
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-01-14 09:52:52 +0100

    Empezado ejercicio1

---
+++ t4p1_asociacion/src/main/java/t4p1_asociacion/T4p1_asociacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package t4p1_asociacion;
+
+/**
+ *
+ * @author Alumno
+ */
+public class T4p1_asociacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
