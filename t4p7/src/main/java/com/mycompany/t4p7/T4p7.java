5b029d7847b2cd8deb334a74f25e75b461027953
Leonardo Batiste Carbonell <leonardo.batiste.estudios@gmail.com>
2026-02-04 13:45:29 +0100

    Añadido las clases del ejercicio

---
+++ t4p7/src/main/java/com/mycompany/t4p7/T4p7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t4p7;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class T4p7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
