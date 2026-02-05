3c40dde8031704557a79299a1e80d3951c83a3e8
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-02-05 09:54:27 +0100

    Creadas las clases

---
+++ t4p8_Interfaces/src/main/java/com/mycompany/t4p8_interfaces/Autobus.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.t4p8_interfaces;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Autobus {
+    
+}
