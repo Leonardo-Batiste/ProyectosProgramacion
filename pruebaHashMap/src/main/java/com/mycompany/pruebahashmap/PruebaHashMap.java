189b3a1748e524b22d5c9310e978a085be016244
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-02-09 10:57:28 +0100

    Terminado ejemplo pruebaHashMap

--- pruebaHashMap/src/main/java/com/mycompany/pruebahashmap/PruebaHashMap.java
+++ pruebaHashMap/src/main/java/com/mycompany/pruebahashmap/PruebaHashMap.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.pruebahashmap;
 
 import java.util.HashMap;
 import java.util.Scanner;
 
-/**
- *
- * @author AluDAM
- */
 public class PruebaHashMap {
 
     public static void main(String[] args) {
-        HashMap<String, String> diccionario = new HashMap <String, String>();
+        HashMap<String, String> diccionario = new HashMap<String, String>();
         String palabra, respuesta;
-        Scanner prompt = new Scanner(System.in); //Cambiar esto por scanner
+        Scanner prompt = new Scanner(System.in); 
         
         diccionario.put("Hola", "Hello");
         diccionario.put("Adios", "Bye");
@@ -26,16 +18,24 @@ public class PruebaHashMap {
         diccionario.put("Mermelada", "Jam");
         diccionario.put("Atasco", "Jam");
         
-        System.out.println("Mini-diccionario Español-Ingles");
+        System.out.println("--- Mini-diccionario Español-Ingles ---");
+        
         do {
-            palabra = prompt.nextLine("Dime una frase en español");
-            if (diccionario.containsKey(palabra)){
-                System.out.println(palabra + " se dice en ingles: "+diccionario.get(palabra));
+            System.out.print("\nDime una palabra en español: ");
+            palabra = prompt.nextLine(); // Leemos la palabra
+            
+            if (diccionario.containsKey(palabra)) {
+                System.out.println(palabra + " se dice en ingles: " + diccionario.get(palabra));
+            } else {
+                System.out.println(palabra + " no se encuentra en el diccionario.");
             }
-            else{
-                System.out.println(palabra+" no se encuentra en el diccionario");
-            }
-            respuesta = prompt.leerString("Quieres buscar otra palabra (S/N): ");
+            
+            System.out.print("¿Quieres buscar otra palabra? (S/N): ");
+            respuesta = prompt.nextLine(); // Leemos la respuesta (S o N)
+            
         } while (respuesta.equalsIgnoreCase("S"));
+        
+        System.out.println("¡Programa finalizado!");
+        prompt.close(); // Cerramos el scanner
     }
 }
