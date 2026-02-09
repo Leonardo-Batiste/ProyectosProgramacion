5bbfa28c343e0d4307a429e48aa934b35276953e
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-02-09 12:30:44 +0100

    Empezado t6p7

---
+++ t6/t6p7_CuentaBancaria/src/main/java/com/mycompany/t6p7_cuentabancaria/CuentaBancaria.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t6p7_cuentabancaria;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class CuentaBancaria {
+    
+    protected int codigo;
+    
+    protected String titular;
+    
+    protected double saldo;
+    
+    public void depositar(){
+    
+    }
+    
+    public void retirar(){
+    
+    }
+    
+}
---
+++ t6/t6p7_CuentaBancaria/src/main/java/com/mycompany/t6p7_cuentabancaria/CuentaCorriente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.t6p7_cuentabancaria;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class CuentaCorriente extends CuentaBancaria {
+    protected double limiteRiesgo;
+    
+    
+}
