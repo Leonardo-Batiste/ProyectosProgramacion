bdb90f4a2cf0253dd0d156eb766b9847721f8243
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-02-11 10:56:38 +0100

    Creado el boilerplate

---
+++ t5/t5p10_ValidacionPasswd/src/main/java/com/my/company/t5p10/model/ContrasenaInvalidaException.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.my.company.t5p10.model;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class ContrasenaInvalidaException {
+    
+}
---
+++ t5/t5p10_ValidacionPasswd/src/main/java/com/mycompany/t5p10_validacionpasswd/T5p10_ValidacionPasswd.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t5p10_validacionpasswd;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class T5p10_ValidacionPasswd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
