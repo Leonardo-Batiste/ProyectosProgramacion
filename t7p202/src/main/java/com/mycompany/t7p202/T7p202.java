2f48572353a8505ce0607c2cdbbb6c154cc4616f
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-02-19 10:49:55 +0100

    Empezado problema

---
+++ t7p202/src/main/java/com/mycompany/t7p202/T7p202.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t7p202;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class T7p202 {
+
+    /**
+     * Crea un programa que:
+     * <ul>
+     * <li>Lea por teclado una cantidad de cadenas de caracteres a insertar.</li>
+     * <li>Genera dicha cantidad de cadenas de caracteres aleatorias a partir de un vector de palabras dado, las inserta en una colección de la clase ArrayList (lista vector) y las inserta también en un colección de la clase LinkedList (lista enlazada). Cada cadena de caracteres a insertar se deberá seleccionar de forma aleatoria a partir de la siguientes palabras: secuencial, aleatorio, letra, número, frío, calor, alto, bajo, dibujo, redacción, grande, pequeño, montaña, rio</li>
+     * <li>Recorra los elementos de la lista vector de cadenas con un iterador y los visualice en consola.</li>
+     * <li>Recorra los elementos de la lista vector de cadenas por posición y los visualice en consola</li>
+     * <li>Visualice en consola la lista enlazada de cadenas usando el método toString.</li>
+     * <li>Visualice en consola la lista vector de cadenas usando el método toString.</li>
+     * <li>Recorra los elementos de la lista enlazada de cadenas con un iterador y los visualice en consola.</li>
+     * <li>Recorra los elementos de la lista enlazada de cadenas por posición y los visualice en consola</li>
+     * </ul>
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
