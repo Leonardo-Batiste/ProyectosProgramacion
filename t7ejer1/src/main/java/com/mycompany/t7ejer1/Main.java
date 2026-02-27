eb5f0eb6246e34c373d5dc657b56fc2eaef464ab
Leonardo-Batiste <leonardo.batiste.estudios@gmail.com>
2026-02-27 20:32:15 +0100

    t7ejer1
    * Creado main
    
    * Creados las distintas clases
    
    * Crear animales y mostrar sus datos
    
    * Cambiado el mostrarDatos a un metodo en Animal
    
    * Añadido eliminar el animal mediante un nombre
    
    * Terminado t7ejer1

---
+++ t7ejer1/src/main/java/com/mycompany/t7ejer1/Main.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t7ejer1;
+
+import java.util.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author leona
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Collection<Animal> listaAnimales = new HashSet<>();
+        
+        boolean condicionWhileCrearAnimales = true;
+        
+        //Este while sirve para simplemente crear los animales
+        while (condicionWhileCrearAnimales){
+            int opcionAnimalParaCrear=Integer.parseInt(JOptionPane.showInputDialog("Que animal deseas crear?"+"\n"
+                                                            + "0 - Ninguno"+"\n"
+                                                            + "1 - Perro"+"\n"
+                                                            + "2 - Gato"+"\n"
+                                                            + "3 - Caballo"));
+
+            switch (opcionAnimalParaCrear){
+                case 0:
+                    condicionWhileCrearAnimales=false;
+                    break;
+
+                case 1:
+                    String nombrePerro=JOptionPane.showInputDialog("Introduce el nombre del perro:");
+                    String razaPerro=JOptionPane.showInputDialog("Introduce la raza del perro:");
+                    
+                    Animal perro = new Perro(nombrePerro, razaPerro);
+                    listaAnimales.add(perro);
+                    JOptionPane.showMessageDialog(null, "Se ha creado el objeto perro correctamente.");
+                    break;
+
+                case 2:
+                    String nombreGato=JOptionPane.showInputDialog("Introduce el nombre del Gato:");
+                    String razaGato=JOptionPane.showInputDialog("Introduce la raza del Gato:");
+                    
+                    Animal gato = new Gato(nombreGato, razaGato);
+                    JOptionPane.showMessageDialog(null, "Se ha creado el objeto Gato correctamente.");
+                    listaAnimales.add(gato);
+                    break;
+
+                case 3:
+                    String nombreCaballo=JOptionPane.showInputDialog("Introduce el nombre del Caballo:");
+                    String razaCaballo=JOptionPane.showInputDialog("Introduce la raza del Caballo:");
+                    
+                    Animal caballo = new Caballo(nombreCaballo, razaCaballo);
+                    listaAnimales.add(caballo);
+                    JOptionPane.showMessageDialog(null, "Se ha creado el objeto Caballo correctamente.");
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Has introducido la creacion de un animal que no es correcto, vuelve a intentarlo.");
+                    break;
+                    
+            }
+        }
+        
+        boolean condicionWhileEliminarAnimales=true;
+        
+        while (condicionWhileEliminarAnimales){
+            
+            int opcionSiQuiereEliminar=Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer?:"+"\n"
+                                                                    + "0 - No eliminar"+"\n"
+                                                                    + "1 - Eliminar un animal"));
+            
+            switch (opcionSiQuiereEliminar){
+                case 0:
+                    condicionWhileEliminarAnimales=false;
+                    break;
+                
+                case 1:
+                    //Mostramos los datos de todos los animales
+                    Animal.mostrarTodosDatos(listaAnimales);
+
+                    //Eliminar animal introducido
+                    String animalParaEliminar=JOptionPane.showInputDialog(null, "Introduce un animal para borrarlo.");
+
+                    Animal.eliminarAnimal(listaAnimales, animalParaEliminar);
+
+                    System.out.println(listaAnimales);
+                    break;
+            }
+        }
+    }
+}
---
+++ t7ejer1/src/main/java/com/mycompany/t7ejer1/Perro.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.t7ejer1;
+
+/**
+ *
+ * @author leona
+ */
+public class Perro extends Animal implements Parlanchin {
+    public Perro(String nombre, String raza){
+        super(nombre, raza);
+    }
+    
+    @Override
+    public String toString(){
+        return "Nombre perro: " + this.nombre + "\n "
+                + "Raza de perro: " + this.raza+"\n"
+                + "Cuidado diario: "+cuidadoDiario()+"\n"
+                + this.nombre + " quiere decir algo: "+habla(); 
+    }
+    
+    @Override
+    public String cuidadoDiario(){
+        return "Pasearlo 3 veces al dia";
+    }
+    
+    @Override
+    public String habla(){
+        return "*ladra* Guau";
+    }
+}
