bab639910a4288fdfc04779a4d258d6b8373a47d
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-03-04 10:50:59 +0100

    Empezado t7ejer11

---
+++ t7ejer11/src/main/java/t7ejer11/Plato.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t7ejer11;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class Plato {
+
+    /**
+     * Será un númeroentre 1 y 3.Siendo 1 primer plato, 2 segundo y 3 postre
+     */
+    protected int descripcion;
+    
+    
+    
+    
+}
