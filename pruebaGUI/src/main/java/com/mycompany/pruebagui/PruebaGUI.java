47b04962edd047f8b2494c9c09ffe7957664a6e1
Leonardo-Batiste <leonardo.batiste.estudios@gmail.com>
2026-03-18 11:09:21 +0100

    Gui guiado
    * Primer ejemplo de asistente de ventanas
    
    * Hecho ejemplo de cambiar el color con 3 textfield

---
+++ pruebaGUI/src/main/java/com/mycompany/pruebagui/PruebaGUI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagui;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class PruebaGUI {
+
+    public static void main(String[] args) {
+        VentanaPrincipal.main(new String[0]);
+        
+    }
+}
