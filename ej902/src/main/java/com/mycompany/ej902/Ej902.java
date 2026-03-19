76e67931d19dd5335e97f63d75602b11ae7f162a
Leonardo-Batiste <leonardo.batiste.estudios@gmail.com>
2026-03-19 09:50:30 +0100

    Terminado ej902
    * Terminado ej902
    
    * Añadido un texto default a colorActual, para que no desaparezca

---
+++ ej902/src/main/java/com/mycompany/ej902/Ej902.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ej902;
+
+/**
+ *
+ * @author leona
+ */
+public class Ej902 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal.main(new String[0]);
+    }
+}
