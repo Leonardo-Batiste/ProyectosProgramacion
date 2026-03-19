e27c10b861ff5b19e2d1c61f13fcb6dbd39fc679
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-03-19 10:27:43 +0100

    Terminado

---
+++ ej803g/src/main/java/com/mycompany/ej803g/Ej803g.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ej803g;
+
+/**
+ *
+ * @author AluDAM
+ */
+public class Ej803g {
+
+    public static void main(String[] args) {
+        VentanaPrincipal.main(new String[0]);
+    }
+}
