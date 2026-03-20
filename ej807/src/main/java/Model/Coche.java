3c8adef65582ce32ed01e32666a8754525666c04
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-03-20 15:43:59 +0100

    Creada clase coche

---
+++ ej807/src/main/java/Model/Coche.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class Coche {
+    
+    protected static int incrementativo = 0;
+    
+    protected String codigo;
+    
+    protected String matricula;
+    
+    protected String modelo;
+    
+    /**
+     * !!!atributo precio es con impuetos incluidos?
+     */
+    protected double precio;
+    
+    protected boolean aLaVenta;
+   
+    protected final double iva = 0.20;
+    
+    public Coche(String matriculaCoche, String modeloCoche, double precioCocheSinImpuestos){
+        
+        this.codigo = "2021-" + ++incrementativo;
+        
+        this.matricula = matriculaCoche;
+        
+        this.modelo = modeloCoche;
+        
+        this.precio += precioCocheSinImpuestos * this.iva;
+        
+        this.aLaVenta = true;
+        
+    }
+    
+    
+}
