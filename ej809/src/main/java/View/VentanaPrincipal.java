01ef19bd06b08093a9fdaf1c06cba023fe3efd60
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-03-24 19:49:30 +0100

    Creado frame VentanaPrincipal

---
+++ ej809/src/main/java/View/VentanaPrincipal.java
@@ -0,0 +1,60 @@
+package View;
+
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    
+    public VentanaPrincipal(){
+        
+        initComponents();
+        
+    }
+    
+    private JFrame mainFrame;
+    private JPanel panelPrincipal;
+    private JButton altaLibro;
+    private JButton altaMultimedia;
+    private JButton fin;
+    
+    private void initComponents(){
+        
+        mainFrame = new JFrame("Bibloteca");
+        
+        panelPrincipal = new JPanel(new FlowLayout());
+        
+        altaLibro = new JButton("Alta libro");
+        
+        altaMultimedia = new JButton("Alta multimedia");
+        
+        fin = new JButton("Fin");
+        
+        panelPrincipal.add(altaLibro);
+        
+        panelPrincipal.add(altaMultimedia);
+        
+        panelPrincipal.add(fin);
+        
+        mainFrame.add(panelPrincipal);
+        
+        mainFrame.pack();
+        
+        mainFrame.setLocationRelativeTo(null);
+        
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+    }
+    
+    public void showFrame(){
+        
+        mainFrame.setVisible(true);
+        
+    }
+    
+    public void hideFrame(){
+        
+        mainFrame.setVisible(false);
+        
+    }
+    
+}
