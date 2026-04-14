02c3350486f34143902fe078ce2898304f86ebf2
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-04-14 09:51:36 +0200

    Hecho el listener de servir material

--- ej908/src/main/java/Controller/MainController.java
+++ ej908/src/main/java/Controller/MainController.java
@@ -1,6 +1,7 @@
 package Controller;
 import View.*;
 import java.awt.event.*;
+import java.util.*;
 import javax.swing.JOptionPane;
 public class MainController {
     public MainController(){
@@ -19,16 +20,30 @@ public class MainController {
         mf.showFrame();
     }
     
+    Map<String, Integer> inventario = new HashMap<>();
+    
     private void crearListenersServirMaterial(){
         mf.getServirMaterial().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                String codigoMaterialIntroducido = mf.getCodigo().getText();
-                boolean materialExiste = false;
+                String codigoIntroducido = mf.getCodigo().getText();
+                int stockIntroducido = Integer.parseInt(mf.getCantidad().getText());
                 
-                if (){
-                    
+                if (!inventario.containsKey(codigoIntroducido)){
+                    JOptionPane.showMessageDialog(null, "No existe ese codigo.");
+                    return;
                 }
+                
+                int stockDisponible = inventario.get(codigoIntroducido);
+                
+                if (stockDisponible < stockIntroducido){
+                    JOptionPane.showMessageDialog(null, "No hay suficiente stock.");
+                    return;
+                }
+                
+                stockDisponible -= stockIntroducido;
+                
+                inventario.replace(codigoIntroducido, stockDisponible);
                 
             }
         });
